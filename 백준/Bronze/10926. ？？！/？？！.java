8f0abd3ca2dcb2eb4ac7150d5e1cd56ee3fbb932
yujinkim31 <qnqn0828@naver.com>
2024-10-12 19:39:25 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String n = "";
+        //String nr = "";
+        n = sc.next();
+        //n.append("??!");
+        System.out.println(n+"??!");
+       
+       
+    }
+}
