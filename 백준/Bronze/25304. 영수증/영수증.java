bdffa90619ee72d4076f871cd59788cbdbdbae90
yujinkim31 <qnqn0828@naver.com>
2024-11-05 19:52:05 +0900

    [Bronze IV] Title: 영수증, Time: 108 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int X = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sum += a * b;
+        }
+        if (sum == X)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
