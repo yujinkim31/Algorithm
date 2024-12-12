12726f2d8e222562b8891e58f4b6c69c16ab1a04
Yujin Kim <qnqn0828@naver.com>
2024-12-12 20:34:51 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.04 ms, Memory: 98.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        // for(int i = 0;i < myString.length;i++) {
+        //     if(myString.substring(i,1))
+        // }
+        answer = myString.toLowerCase();
+        
+        return answer;
+    }
+}
